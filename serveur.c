b2b1aefc1756409e75571863d136773b55bbb903
pas2406 <papaaliounes83@gmail.com>
2026-05-02 01:43:35 +0200

    feat(server): implement create_listening_sock and basic echo

--- serveur.c
+++ serveur.c
@@ -10,6 +10,7 @@
 #include "user.h"
 
 #define PORT_FREESCORD 4321
+#define NB_CLIENTS 124
 
 /** Gérer toutes les communications avec le client renseigné dans
  * user, qui doit être l'adresse d'une struct user */
@@ -20,7 +21,64 @@ int create_listening_sock(uint16_t port);
 
 int main(int argc, char *argv[])
 {
-	return 0;
+	
+	
+	int sock_l = create_listening_sock(PORT_FREESCORD);
+	if(sock_l < 0){
+		fprintf(stderr, "Erreur lors de la création de la socket d'écoute\n");
+		exit(1);
+	}
+	
+	
+	
+	while(1){
+		
+		char buf[256];
+		ssize_t n;
+		struct sockaddr_in sa_clt;
+		socklen_t sl = sizeof(sa_clt);
+		
+
+		int fds_clt = accept(sock_l, (struct sockaddr *) &sa_clt, &sl);
+		if (fds_clt< 0) { 
+			perror("accept");
+			close(sock_l);
+			exit(1);
+		}
+		
+
+		while (1)
+		{
+			
+			if ((n = read(fds_clt, buf, 256)) < 0) {
+				perror("read");
+				break;
+	
+			}
+	
+			
+			if(n == 0){
+				fprintf(stderr, "Client déconnecté\n");
+				break;
+			}
+			
+			//printf("Message reçu : %s\n", buf);	
+			if((write(fds_clt, buf, n)) < 0){
+				perror("write");
+				break;
+			}
+		}
+
+		close(fds_clt);
+		
+
+
+
+
+	}
+	close(sock_l);
+
+
 }
 
 void *handle_client(void *clt)
@@ -30,5 +88,33 @@ void *handle_client(void *clt)
 
 int create_listening_sock(uint16_t port)
 {
-	return port;
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0){
+		perror("socket");
+		close(sock);
+		return(-1);
+	}
+
+	struct sockaddr_in s_addr;
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(port);
+	s_addr.sin_addr.s_addr = INADDR_ANY;
+
+	int opt = 1;
+	// Permet de réutiliser l'adresse même si elle est encore en TIME_WAIT
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
+
+	//on attache la socket à l'adresse et au port
+	if (bind(sock, (struct sockaddr *) &s_addr, sizeof(s_addr)) < 0) {
+		perror("bind");
+		close(sock);
+		return(-1);
+	}
+
+	if(listen(sock, NB_CLIENTS) < 0){
+		perror("listen");
+		close(sock);
+		return(-1);
+	}
+	return sock;
 }
