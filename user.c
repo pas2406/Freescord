6c77504c3fb24049ac4f09a06d9e4466493eded1
pas2406 <papaaliounes83@gmail.com>
2026-05-09 14:10:36 +0200

    accept_user and free_user

--- user.c
+++ user.c
@@ -9,14 +9,34 @@
  * initialisé */
 struct user *user_accept(int sl)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	sl = 0;
-	return NULL;
+
+	struct user * usr = malloc(sizeof(struct user));
+
+	if(! usr){
+		perror("malloc");
+		exit(1);
+	}
+
+	usr->address = malloc(sizeof(struct sockaddr));
+	if(!(usr->address)){
+		perror("malloc");
+		exit(1);
+	}
+
+	usr->sock =  accept(sl, usr->address, &(usr->addr_len));
+	if(usr->sock < 0){
+		perror("accept");
+		exit(1);
+	}
+
+	return usr;
+	
 }
 
 /** libérer toute la mémoire associée à user */
 void user_free(struct user *user)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	user = NULL;
+	free(user-> address);
+	free(user);
+
 }
