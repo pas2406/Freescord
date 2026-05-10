b7765239e4139b40f9d565b395adcb4492bd7e6c
pas2406 <papaaliounes83@gmail.com>
2026-05-10 20:50:22 +0200

    implementation de la bibliotheque buffer.h et utils.h

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -2,63 +2,176 @@
 
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 struct buffer {
 	int fd;
-	/* autres champs, à vous */
+	size_t buffsz;
+	char * buf;
+	size_t cursor;
+	size_t valid_sz; //taille des caracteres valides
+	int eof; // 1 si la fin du fichier est atteinte , 0 sinon
+	
 };
 
 buffer *buff_create(int fd, size_t buffsz)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	buffsz = fd = 0;
-	return NULL;
+	buffer * res = malloc(sizeof(buffer));
+	if(!res) {
+		perror("malloc");
+		return NULL;
+	}
+
+	res -> fd = fd;
+	res -> buffsz = buffsz;
+	res -> cursor = 0;
+	res -> valid_sz = 0;
+	res -> eof = 0;
+
+	res -> buf = malloc(sizeof(char) * buffsz);
+	if(!(res -> buf)){
+		perror("malloc");
+		free(res);
+		return NULL;
+	}
+
+	return res;
 }
 
 int buff_getc(buffer *b)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	b = NULL;
-	return 0;
+	size_t n;
+
+	if(b->eof == 1){
+		return EOF;
+	}
+	if(b -> cursor >=  b -> valid_sz){ //tous les caractere valides du buffer sont consommés
+		if((n = read(b->fd, b->buf, b->buffsz)) == 0){
+			b ->eof = 1;
+			return EOF;
+		}
+
+		if (n < 0){
+			perror("read");
+			return EOF;
+		}
+		b->valid_sz = n;
+		b->cursor = 0;
+		
+	
+	}
+
+	return b->buf[b->cursor++];
+
+
 }
 
 int buff_ungetc(buffer *b, int c)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	b = NULL;
+	if (b->cursor == 0){
+		return EOF;
+	}
+
+	
+	b->cursor--;
+	b->buf[b->cursor] = c;
+
 	return c;
 }
 
 void buff_free(buffer *b)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	b = NULL;
+	free(b->buf);
+	free(b);
 }
 
 int buff_eof(const buffer *buff)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	return buff == NULL;
+	
+	return buff-> eof;
 }
 
 int buff_ready(const buffer *buff)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	return buff == NULL;
+	
+	if((buff->valid_sz) > 0 && (buff->cursor)  <  (buff->valid_sz)){
+		return 1;
+	}
+	return 0;
 }
+
 
 char *buff_fgets(buffer *b, char *dest, size_t size)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	b = NULL; size = 0;
-	return dest;
+	int i = 0;
+	int c;
+	while(i < (size - 1)){
+		c = buff_getc(b);
+		
+		if(c == EOF){ //si on atteint la fin du fichier
+			if(i == 0){
+				return NULL; // sans que des caracteres soient lus
+			}else{
+				
+				break;
+			}	
+
+		}else if (c == '\n'){
+			dest[i] = c;
+			i++;
+			break;
+			
+		}
+		dest[i] = c;
+		i++;
+		
+	}
+	dest[i] = '\0';
+	
+	return(dest);
+	
 }
+
+
 
 char *buff_fgets_crlf(buffer *b, char *dest, size_t size)
 {
-	/* pour éviter les warnings de variable non utilisée */
-	b = NULL; size = 0;
-	return dest;
+	int i = 0;
+	int c1, c2;
+	while(i < (size - 2)){
+		c1 = buff_getc(b);
+
+		
+		if(c1 == EOF){ //si on atteint la fin du fichier
+			if(i == 0){ 
+				return NULL; // sans que des caracteres soient lus
+			}else{
+				break;
+			}	
+			
+		}else if (c1 == '\r'){
+			if((c2 = buff_getc(b)) == '\n'){
+				dest[i] = c1;
+				dest[i + 1] = c2; 
+				i+=2;
+				break;
+
+			}else{
+				if(c2 != EOF){
+					buff_ungetc(b, c2);
+				}
+			}	
+			
+			
+		}
+		
+		dest[i] = c1;
+		i++;
+			
+	}
+	dest[i] = '\0';
+	return(dest);
+	
 }
--- utils.c
+++ utils.c
@@ -1,12 +1,60 @@
 #include <string.h>
+
+/* Changer une ligne se terminant par CRLF en une ligne se terminant par LF.
+ * La ligne doit être terminée par un carcactère nul.
+ * Retourne la ligne modifiée ou NULL en cas d'erreur. */
 char *crlf_to_lf(char *line_with_crlf)
 {
-	/* pour éviter les warnings de variable non utilisée */
+
+	
+	if(! line_with_crlf){
+		return NULL;
+	}
+
+	int len = strlen(line_with_crlf);
+	if(len < 2){
+		return NULL;
+	}
+	
+	
+	//on verifie si on a bien une ligne qui se termine par crlf
+	if(line_with_crlf[len - 1] != '\n' || line_with_crlf[len - 2] != '\r' ){
+		return NULL;
+	}
+
+	line_with_crlf[len - 2] = '\n';
+	line_with_crlf[len - 1] = '\0';
+
 	return line_with_crlf;
 }
 
+
+/* Changer une ligne se terminant par LF en une ligne se terminant par CRLF.
+ * La ligne doit être terminée par un carcactère nul.
+ * Attention, l'utilisateur doit vérifier que le tableau peut contenir
+ * un caractère supplémentaire.
+ * Retourne la ligne modifiée ou NULL en cas d'erreur. */
 char *lf_to_crlf(char *line_with_lf)
 {
-	/* pour éviter les warnings de variable non utilisée */
+
+	if( !line_with_lf){
+		return NULL;
+	}
+
+	int len = strlen(line_with_lf);
+
+	if(len < 1){
+		return NULL;
+	}
+
+	if(line_with_lf[len - 1] != '\n'){
+		return NULL;
+	}
+
+
+	line_with_lf[len - 1] = '\r';
+	line_with_lf[len] = '\n';
+	line_with_lf[len + 1] = '\0';
+
 	return line_with_lf;
 }
